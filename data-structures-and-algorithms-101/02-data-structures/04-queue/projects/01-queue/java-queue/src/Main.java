3e4b17f29ff52914976a0b6be68f35c9f557bc50
malekcoders <alomayri970@outlook.com>
2023-12-13 10:51:20 +0300

    add simple queue project

--- data-structures-and-algorithms-101/02-data-structures/04-queue/projects/01-queue/java-queue/src/Main.java
+++ data-structures-and-algorithms-101/02-data-structures/04-queue/projects/01-queue/java-queue/src/Main.java
@@ -3,45 +3,6 @@ import QueueLinkedList.QueueLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-
-        /**
-            @QueueArray
-
-        QueueArray queue = new QueueArray();
-
-        queue.enQueue(10);
-        queue.enQueue(20);
-        queue.enQueue(30);
-        queue.enQueue(40);
-        queue.enQueue(50);
-        queue.enQueue(60);
-
-        queue.display();
-
-        queue.deQueue();
-
-        queue.display();
-        */
-
-        /**
-            @QueueLinkedList
-        */
-        QueueLinkedList queue = new QueueLinkedList();
-
-        queue.display();
-        queue.enQueue(10);
-        queue.enQueue(20);
-
-        queue.display();
-        queue.deQueue();
-        queue.enQueue(30);
-        queue.enQueue(40);
-        queue.enQueue(50);
-        queue.display();
-
-
-
-
-
+        
     }
 }
