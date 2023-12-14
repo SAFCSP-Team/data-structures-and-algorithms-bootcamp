eb48ebc89007c0ff1d85faa74a43953c254699e1
fahadLine <alyaqubf@gmail.com>
2023-12-14 14:38:33 +0300

    Uodates

---
+++ data-structures-and-algorithms-101/02-data-structures/05-tree/projects/introduction-to-tree/01-premetive-tree-creation/BinaryTree.java
@@ -0,0 +1,22 @@
+
+class Node {
+ 
+  // * add your code here *
+
+  // attributes declaraion
+  
+  // Node constructre 
+
+}
+
+
+
+class BinaryTree {
+
+  // * add your code here *
+
+  // attributes declaraion
+  
+  // BinaryTree constructres 
+
+}
