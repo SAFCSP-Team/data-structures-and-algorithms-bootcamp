4bcbed839d8f62a17a98f3391da25eed3bcf8d01
fahadLine <152792332+fahadLine@users.noreply.github.com>
2023-12-18 13:15:52 +0300

    Update TreeNode.java

--- data-structures-and-algorithms-101/02-data-structures/05-tree/projects/introduction-to-tree/01-premetive-tree-creation/TreeNode.java
+++ data-structures-and-algorithms-101/02-data-structures/05-tree/projects/introduction-to-tree/01-premetive-tree-creation/TreeNode.java
@@ -4,37 +4,41 @@ import java.util.ArrayList;
 
 public class TreeNode {
 
-    // Node Data
-    int number;
+    // Node data
+    // ** add your code here **
+    // 1 - 
+
     // Node Children
     ArrayList<TreeNode> children = new ArrayList<TreeNode>();
 
     // Constructor to create the node
-    TreeNode(int number){
-        this.number = number;
+    TreeNode(int name){
+        this.name = name;
     }
 
     public static void main(String[] args) {
         
         // Create the root
-        TreeNode root = new TreeNode(1);
+        TreeNode root = new TreeNode("Mohammed");
 
         // Create TreeNodes
-        TreeNode child1 = new TreeNode(2);
-        TreeNode child2 = new TreeNode(3);
-        TreeNode child3 = new TreeNode(4);
+        TreeNode child1 = new TreeNode("Ahmed");
+         // ** add your code here **
+        // 2 - 
+
 
         // Add the TreeNodes as the root children
         root.children.add(child1);
-        root.children.add(child2);
-        root.children.add(child3);
+           // ** add your code here **
+        // 3 - 
+
 
         // Print the root and the children data
         System.out.println(root.number);
-        System.out.println(root.children.get(0).number);
-        System.out.println(root.children.get(1).number);
-        System.out.println(root.children.get(2).number);
-
+        System.out.println(root.children.get(0).name);
+        System.out.println(root.children.get(1).name);
+            // ** add your code here **
+        // 4 - 
     }
 
     
