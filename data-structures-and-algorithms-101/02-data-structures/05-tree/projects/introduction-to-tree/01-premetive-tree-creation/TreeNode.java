6dc2e0316a832c5bcaa852768b8f7b0ef1c94d10
fahadLine <152792332+fahadLine@users.noreply.github.com>
2023-12-18 13:20:55 +0300

    Update TreeNode.java

--- data-structures-and-algorithms-101/02-data-structures/05-tree/projects/introduction-to-tree/01-premetive-tree-creation/TreeNode.java
+++ data-structures-and-algorithms-101/02-data-structures/05-tree/projects/introduction-to-tree/01-premetive-tree-creation/TreeNode.java
@@ -6,7 +6,7 @@ public class TreeNode {
 
     // Node data
     // ** add your code here **
-    // 1 - 
+    // 1 - Declare name attribute as String
 
     // Node Children
     ArrayList<TreeNode> children = new ArrayList<TreeNode>();
@@ -24,13 +24,13 @@ public class TreeNode {
         // Create TreeNodes
         TreeNode child1 = new TreeNode("Ahmed");
          // ** add your code here **
-        // 2 - 
+        // 2 - craete {child2} with the name Khalid and {child3} with the name Abdullah 
 
 
         // Add the TreeNodes as the root children
         root.children.add(child1);
            // ** add your code here **
-        // 3 - 
+        // 3 - add {child2} and {child3} to the root children 
 
 
         // Print the root and the children data
@@ -38,7 +38,7 @@ public class TreeNode {
         System.out.println(root.children.get(0).name);
         System.out.println(root.children.get(1).name);
             // ** add your code here **
-        // 4 - 
+        // 4 - print root child2 name
     }
 
     
