003e3975c4a6396f6694061a492630d1cd553ab2
lamabeta <148013077+lamabeta@users.noreply.github.com>
2023-12-20 17:35:31 +0300

    Delete data-structures-and-algorithms-101/02-data-structures/01-arrays/projects/03-multi-dimensional-arrays directory

--- data-structures-and-algorithms-101/02-data-structures/01-arrays/projects/03-multi-dimensional-arrays/src/ConverDimensional.java
+++
@@ -1,9 +0,0 @@
-public class ConverDimensional {
-   public static int[][] conver(int[] nums) {
-              /*add your code */
-   }
-  public static void main(String[] args) {
-       int[] nums = {2,9,5,3,8,1,4,6,7};
-      /*call the method and print array here*/
- }
-}
